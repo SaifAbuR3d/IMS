b06f123ea9e8a80313097053bc94e4ba03bf1d81
SaifAbuR3d <saefraed2@gmail.com>
2023-07-23 20:00:19 +0300

    Add project files.

---
+++ IMS.BL/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.BL
+{
+    public class Inventory
+    {
+        public Inventory()
+        {
+            Products = new List<Product>();
+        }
+        public List<Product> Products { get; set; }
+    }
+}
