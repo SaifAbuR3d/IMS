be23c1535e7c9eb9e720f00fd6071b798b795dfa
SaifAbuR3d <saefraed2@gmail.com>
2023-07-24 14:46:51 +0300

    Move class Program to a new namespace.

--- IMS.BL/Program.cs
+++ IMS.BL/Program.cs
@@ -1,63 +1,66 @@
 ﻿using IMS.BL;
 
-public class Program
+namespace IMS.UI
 {
-    static void Main()
+    public class Program
     {
-        Console.WriteLine("----------------  Inventory Management System ----------------\n\n");
-
-        Inventory inventory = new();
-        bool exit = false;
-
-        do
+        static void Main()
         {
-            Console.WriteLine("[1]. Add a product");
-            Console.WriteLine("[2]. View all products");
-            Console.WriteLine("[3]. Edit a product");
-            Console.WriteLine("[4]. Delete a product");
-            Console.WriteLine("[5]. Search for a product");
-            Console.WriteLine("[6]. Exit");
+            Console.WriteLine("----------------  Inventory Management System ----------------\n\n");
 
-            int choice = UserInputHandler.GetValidInt("Enter your choice:  ");
+            Inventory inventory = new();
+            bool exit = false;
 
-            switch (choice)
+            do
             {
-                case 1:
-                    string name = UserInputHandler.GetValidString("Enter product name: ");
-                    decimal price = UserInputHandler.GetValidDecimal("Enter product price: ");
-                    int quantity = UserInputHandler.GetValidInt("Enter product quantity: ");
+                Console.WriteLine("[1]. Add a product");
+                Console.WriteLine("[2]. View all products");
+                Console.WriteLine("[3]. Edit a product");
+                Console.WriteLine("[4]. Delete a product");
+                Console.WriteLine("[5]. Search for a product");
+                Console.WriteLine("[6]. Exit");
 
-                    inventory.AddProduct(name, price, quantity);
-                    break;
-                case 2:
-                    inventory.ViewProducts();
-                    break;
-                case 3:
-                    string productToEdit = UserInputHandler.GetValidString("Enter product name to edit: ");
-                    string newName = UserInputHandler.GetValidString("Enter new product name if wanted, otherwise enter -1: ");
-                    decimal newPrice = UserInputHandler.GetValidDecimal("Enter new product price if wanted, otherwise enter -1: ");
-                    int newQuantity = UserInputHandler.GetValidInt("Enter new product quantity if wanted, otherwise enter -1: ");
+                int choice = UserInputHandler.GetValidInt("Enter your choice:  ");
 
-                    inventory.UpdateProduct(productToEdit, newName, newPrice, newQuantity);
-                    break;
-                case 4:
-                    string productToDelete = UserInputHandler.GetValidString("Enter product name to delete: "); 
-                    inventory.RemoveProduct(productToDelete);
-                    break;
-                case 5:
-                    string productToSearch = UserInputHandler.GetValidString("Enter product name to search: ");
-                    inventory.SearchProduct(productToSearch);
-                    break;
-                case 6:
-                    exit = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
-            }
+                switch (choice)
+                {
+                    case 1:
+                        string name = UserInputHandler.GetValidString("Enter product name: ");
+                        decimal price = UserInputHandler.GetValidDecimal("Enter product price: ");
+                        int quantity = UserInputHandler.GetValidInt("Enter product quantity: ");
 
-            Console.WriteLine();
-            Console.WriteLine();
-        } while (!exit);
+                        inventory.AddProduct(name, price, quantity);
+                        break;
+                    case 2:
+                        inventory.ViewProducts();
+                        break;
+                    case 3:
+                        string productToEdit = UserInputHandler.GetValidString("Enter product name to edit: ");
+                        string newName = UserInputHandler.GetValidString("Enter new product name if wanted, otherwise enter -1: ");
+                        decimal newPrice = UserInputHandler.GetValidDecimal("Enter new product price if wanted, otherwise enter -1: ");
+                        int newQuantity = UserInputHandler.GetValidInt("Enter new product quantity if wanted, otherwise enter -1: ");
+
+                        inventory.UpdateProduct(productToEdit, newName, newPrice, newQuantity);
+                        break;
+                    case 4:
+                        string productToDelete = UserInputHandler.GetValidString("Enter product name to delete: ");
+                        inventory.RemoveProduct(productToDelete);
+                        break;
+                    case 5:
+                        string productToSearch = UserInputHandler.GetValidString("Enter product name to search: ");
+                        inventory.SearchProduct(productToSearch);
+                        break;
+                    case 6:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            } while (!exit);
+        }
     }
 }
